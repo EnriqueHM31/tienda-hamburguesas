677d3b6240ee0c161fd2417363a2110fd34f2bba
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-16 23:02:48 -0600

    Creacion de la seccion Conocenos

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,7 +1,7 @@
 export const LINKS_NAVS = [
 	{ texto: 'Inicio', url: '/' },
 	{ texto: 'Productos', url: '/' },
-	{ texto: 'Conocenos', url: '/' },
+	{ texto: 'Conocenos', url: '#seccion-conocenos' },
 	{ texto: 'Contacto', url: '/' },
 ]
 
@@ -13,4 +13,8 @@ export const IMAGENES_SLIDER = [
 	{ id: 1, url: '../src/assets/img/hamburguesa1.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '100', check: true },
 	{ id: 2, url: '../src/assets/img/hamburguesa2.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '140', check: false },
 	{ id: 3, url: '../src/assets/img/hamburguesa3.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '180', check: false },
+]
+
+export const SOBRE_NOSOTROS = [
+	'En Tienda Mega, nos apasiona ofrecer una experiencia única para los amantes de las hamburguesas. Desde el primer bocado, nuestro objetivo es sorprenderte con sabores auténticos y frescos. Trabajamos con ingredientes de alta calidad, seleccionados cuidadosamente para crear combinaciones deliciosas que satisfacen todos los gustos. ',
 ]
