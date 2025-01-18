87e0a9a5d794292cf09176fec52d28e89292d1d6
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-18 16:55:24 -0600

    Añadir una constante de las iimagenes para la galeria

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -2,6 +2,7 @@ export const LINKS_NAVS = [
 	{ texto: 'Inicio', url: '/#inicio' },
 	{ texto: 'Productos', url: '/#productos' },
 	{ texto: 'Conocenos', url: '#seccion-conocenos' },
+	{ texto: 'Galeria', url: '#galeria' },
 	{ texto: 'Contacto', url: '/' },
 ]
 
@@ -10,11 +11,23 @@ export const DESCRIPCION_TIENDA = [
 ]
 
 export const IMAGENES_SLIDER = [
-	{ id: 1, url: '../src/assets/img/hamburguesa1.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '100', check: true },
-	{ id: 2, url: '../src/assets/img/hamburguesa2.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '140', check: false },
-	{ id: 3, url: '../src/assets/img/hamburguesa3.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '180', check: false },
+	{ id: 1, url: '../src/assets/img/slider/hamburguesa1.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '100', check: true },
+	{ id: 2, url: '../src/assets/img/slider/hamburguesa2.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '140', check: false },
+	{ id: 3, url: '../src/assets/img/slider/hamburguesa3.webp', alt: 'Imagen de la especialidad de la tienda Hamburguesa Mega', precio: '180', check: false },
 ]
 
 export const SOBRE_NOSOTROS = [
 	'En Tienda Mega, nos apasiona ofrecer una experiencia única para los amantes de las hamburguesas. Desde el primer bocado, nuestro objetivo es sorprenderte con sabores auténticos y frescos. Trabajamos con ingredientes de alta calidad, seleccionados cuidadosamente para crear combinaciones deliciosas que satisfacen todos los gustos. ',
+]
+
+export const GALERIA = [
+	{ id: 1, url: '../src/assets/img/galeria/galeria1.webp', alt: 'Imagen parte de la galeria 1' },
+	{ id: 2, url: '../src/assets/img/galeria/galeria2.webp', alt: 'Imagen parte de la galeria 2' },
+	{ id: 3, url: '../src/assets/img/galeria/galeria3.webp', alt: 'Imagen parte de la galeria 3' },
+	{ id: 4, url: '../src/assets/img/galeria/galeria4.webp', alt: 'Imagen parte de la galeria 4' },
+	{ id: 5, url: '../src/assets/img/galeria/galeria5.webp', alt: 'Imagen parte de la galeria 5' },
+	{ id: 6, url: '../src/assets/img/galeria/galeria6.webp', alt: 'Imagen parte de la galeria 6' },
+	{ id: 7, url: '../src/assets/img/galeria/galeria7.webp', alt: 'Imagen parte de la galeria 7' },
+	{ id: 8, url: '../src/assets/img/galeria/galeria8.webp', alt: 'Imagen parte de la galeria 8' },
+	{ id: 9, url: '../src/assets/img/galeria/galeria9.webp', alt: 'Imagen parte de la galeria 9' },
 ]
