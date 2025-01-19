aa20cf0a8e7a4ca84f469014412f2605c5320b7e
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-19 13:08:32 -0600

    estrcutura de carrito

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,3 +1,48 @@
-export default function Carrito() {
-	return <h1>Carrito</h1>
+export default function Carrito({ carrito, handleClickAgregarCantdad, handleClickQuitarCantidad, total }) {
+	return (
+		<>
+			<div className="page">
+				<h1>Carrito de compras</h1>
+
+				<div className="contenedor-productos">
+					{carrito.map(({ id, nombre, precio, descripcion, cantidad }) => {
+						return (
+							<article className="producto-elegido" key={id}>
+								<img src={`/src/assets/img/menu/menu${id}.webp`} alt={nombre} />
+								<div className="contenido-producto-elegido">
+									<div className="contenedor-contenido-producto-elegido">
+										<h2 className="nombre-producto-elegido">{nombre}</h2>
+										<span>${precio}</span>
+									</div>
+
+									<p className="precio-producto-elegido">{descripcion}</p>
+								</div>
+
+								<div className="cantidad-producto">
+									<span>{cantidad}</span>
+									<div className="contenedor-btn-cantidad">
+										<button onClick={() => handleClickAgregarCantdad(id)} className="btn-agregar">
+											<img src="./src/assets/img/ic_agregar.webp" alt="icono para agregar" />
+										</button>
+										<button onClick={() => handleClickQuitarCantidad(id)} className="btn-quitar">
+											<img src="./src/assets/img/ic_quitar.webp" alt="icono para quitar" />
+										</button>
+									</div>
+								</div>
+							</article>
+						)
+					})}
+				</div>
+
+				<div className=" pago-total">
+					<div className="contenido-pago">
+						<h2>Total:</h2>
+						<span>${total}</span>
+					</div>
+
+					<button>Pagar</button>
+				</div>
+			</div>
+		</>
+	)
 }
