73e3538adbfe6e70b98d1e4543e52d4df5f43f34
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-19 20:12:54 -0600

    añadir el valor categoria en cada objeto

--- src/hooks/producto.js
+++ src/hooks/producto.js
@@ -1,8 +1,30 @@
 import { useState } from 'react'
 import { productos as Menu } from '../mook/productos'
+import { CATEGORIAS } from '../utils/constantes'
 
 export default function useProducto() {
-	const [productos] = useState(Menu)
+	const [productos, setProductos] = useState(Menu)
+	const [categorias, setCategorias] = useState(CATEGORIAS)
 
-	return { productos }
+	const handleClickCategoria = (id) => {
+		// Resetear el estado de 'check' de todas las categorías
+		categorias.forEach((item) => (item.check = false))
+
+		// Establecer 'check' en true para el id seleccionado
+		const updatedCategorias = categorias.map((item) => (item.id === id ? { ...item, check: true } : item))
+
+		// Filtrar los productos según la categoría seleccionada
+		const categoriaSeleccionada = updatedCategorias.find((item) => item.id === id)
+		const filteredProductos =
+			categoriaSeleccionada?.nombre === 'Todo'
+				? Menu // Si la categoría seleccionada es 'Todos', mostrar todos los productos
+				: Menu.filter((item) => item.categoria === categoriaSeleccionada?.nombre)
+
+		console.log(filteredProductos)
+		// Actualizar el estado de productos y las categorías
+		setCategorias(updatedCategorias)
+		setProductos(filteredProductos)
+	}
+
+	return { productos, categorias, handleClickCategoria }
 }
