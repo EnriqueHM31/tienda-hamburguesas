a3ed31ba3bc864e731c9a9b8f84dd4bd29f9f492
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-21 16:10:43 -0600

    Añadi los patterns a los inputs del formulario

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -37,9 +37,9 @@ export default function Contacto() {
 				<h2>Formulario de contacto</h2>
 
 				<div className="contenido-formulario">
-					<input type="text" placeholder="Nombre" required />
-					<input type="text" placeholder="Email" required />
-					<textarea placeholder="Mensaje" required></textarea>
+					<input pattern="[A-Za-z ]+" type="text" placeholder="Nombre" required />
+					<input pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.(com)$" type="email" placeholder="Email" required />
+					<textarea pattern="[A-Za-z0-9 ]+" placeholder="Mensaje" required></textarea>
 					<button>Enviar</button>
 				</div>
 				<img src="/src/assets/img/logo.svg" alt="logo" className="logo-contacto" />
