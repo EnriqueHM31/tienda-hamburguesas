7f60824a134f0eee1b5d6ed66c4d94a60cf3276f
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-20 18:01:23 -0600

    Refactorizando la seccion de Galeria

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -6,15 +6,19 @@ export default function Galeria() {
 			<h2>Nuestra Galeria</h2>
 			<div className="galeria-img">
 				{GALERIA.map(({ id, url, alt }) => {
-					return (
-						<div className={`galeria-img-item galeria-${id}`} key={id}>
-							<img src={url} alt={alt} />
-						</div>
-					)
+					return <ImageGrid id={id} url={url} alt={alt} key={id} />
 				})}
 
 				<img className="logo-grid" src="./src/assets/img/logo.svg" alt="Logo de la tienda de la hamburguesas" />
 			</div>
 		</section>
+	)
+}
+
+export function ImageGrid({ id, url, alt }) {
+	return (
+		<div className={`galeria-img-item galeria-${id}`} key={id}>
+			<img src={url} alt={alt} />
+		</div>
 	)
 }
